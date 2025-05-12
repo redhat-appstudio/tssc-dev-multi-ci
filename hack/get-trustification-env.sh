31512c8f82071aa400ae45173d9f8e7baa28676a
Romain Arnaud <rarnaud@redhat.com>
2025-05-12 14:16:31 -0400

    feat: Rename RHTAP to TSSC
    Rename cluster resources.
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- hack/get-trustification-env.sh
+++ hack/get-trustification-env.sh
@@ -7,8 +7,8 @@ set -o errexit -o nounset -o pipefail
 # - Set the generated env vars before running the promotion pipeline
 #   - e.g. with `eval "$(hack/get-trustification-env.sh)"`
 
-TPA_NAMESPACE=${TPA_NAMESPACE:-'rhtap-tpa'}
-KEYCLOAK_NAMESPACE=${KEYCLOAK_NAMESPACE:-'rhtap-keycloak'}
+TPA_NAMESPACE=${TPA_NAMESPACE:-'tssc-tpa'}
+KEYCLOAK_NAMESPACE=${KEYCLOAK_NAMESPACE:-'tssc-keycloak'}
 
 declare -A trustification_env
 
@@ -16,7 +16,7 @@ trustification_env=(
     [BOMBASTIC_API_URL]="https://$(oc -n "$TPA_NAMESPACE" get route --selector app.kubernetes.io/name=bombastic-api -o jsonpath='{.items[].spec.host}')"
     [OIDC_ISSUER_URL]="https://$(oc -n "$KEYCLOAK_NAMESPACE" get route --selector app=keycloak -o jsonpath='{.items[].spec.host}')/realms/chicken"
     [OIDC_CLIENT_ID]=walker
-    [OIDC_CLIENT_SECRET]=$(oc -n rhtap-tpa get secret tpa-realm-chicken-clients -o go-template='{{.data.walker | base64decode}}')
+    [OIDC_CLIENT_SECRET]=$(oc -n tssc-tpa get secret tpa-realm-chicken-clients -o go-template='{{.data.walker | base64decode}}')
     [SUPPORTED_CYCLONEDX_VERSION]=1.4
 )
 
